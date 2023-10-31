91d5487010d489a39b4e52429a9874f0c1cd2503
Juan David Gonzalez <juandagv95@gmail.com>
2023-10-31 00:13:30 -0500

    Add realtime.handlebars, metodo Get y Delete, ademas de correcta config del socket

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,35 @@
+//cliente
 const socket = io();
+socket.on("getProducts", (products) => {
+  console.log("getProducts", products);
+  const contenedorCard = document.getElementById('contenedorCard');
+  
+  contenedorCard.innerHTML = "";
 
-const deleteProduct = document.getElementById('deleteProduct');
-
-socket.emit('message', 'Hello from the client 🖐️.');
-
-socket.on('client-emit', (data) => {
-  console.log('event client-emit', data);
+  products.forEach(product => {
+    const productCard = document.createElement("section");
+    productCard.className = "card";
+    productCard.innerHTML = `
+      <img src="../img/Star_Wars_Logo.svg.png" alt="Imagen de prueba" class="card-img">
+      <h2>${product.title}</h2>
+      <p><span>Categoria</span>: ${product.category}</p>
+      <article class="card-price">
+        <p><span>Precio</span>: ${product.price}</p>
+        <p><span>Stock</span>: ${product.stock}</p>   
+      </article>
+      <p><span>Cod. producto</span>: ${product.code}</p>
+      <button id="deleteProduct${product.id}">Eliminar Producto</button>
+    `;
+    contenedorCard.appendChild(productCard);
+  });
+  products.forEach(product => {
+    const deleteButton = document.getElementById(`deleteProduct${product.id}`);
+    deleteButton.addEventListener('click', (event) => {
+      socket.emit('deleteProduct', product.id);
+    });
+  });
 });
 
-socket.on('broadcast-emit', (data) => {
-  console.log('event broadcast-emit', data);
+socket.on('productDeleted', (productId) => {
+ console.log(`Producto eliminado ${productId}`)
 });
-
--- src/socket.js
+++ src/socket.js
@@ -1,18 +1,31 @@
+//Servidor
 import { Server } from 'socket.io'
+import path from 'path';
+import { __dirname } from './utils.js';
+import ProductManager from './productManager.js';
+
+let io;
 
 export const init = (httpServer) => {
-  const socketServer = new Server(httpServer);
 
-  socketServer.on('connection', (socketClient) => {
+  io = new Server(httpServer);
 
-    console.log(`Nuevo cliente socket conectado ${socketClient.id} 🎊`);
+  io.on('connection', async (socketClient)=>{
+    const productManager = new ProductManager(path.join(__dirname, './Products.json'));
+    const products = await productManager.getProducts();
+    socketClient.emit('getProducts', products);
 
-    socketClient.emit('client-emit', { status: "ok" });
-    socketClient.broadcast.emit('broadcast-emit', { status: "ok" });
-    socketServer.emit('all-clients', { status: "ok" });
-
-    socketClient.on('message', (msg) => {
-      console.log(`Cliente envio un nuevo mensaje: ${msg}`);
-    });
-  });
-} 
+    socketClient.on('deleteProduct', async (productId) => {
+      try {
+        // Elimina el producto en el servidor utilizando la función deleteProduct
+        await productManager.deleteProduct(productId);
+        
+        // Emita un evento para notificar a todos los clientes que el producto ha sido eliminado
+        io.emit('productDeleted', productId);
+      } catch (error) {
+        console.error('Error al eliminar el producto:', error);
+        // Manejar errores, por ejemplo, emitir un evento de error
+      }
+    });  
+  })
+}; 
